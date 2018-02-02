93efb6e66db4dbbdc9f34e2f70f6c019c21bf31e
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2018-02-02 10:09:49 -0800

    Add GraphQL Schema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const fs = require('fs');
+const graphql = require('graphql');
 
 const app = express();
+const myGraphQLSchema = graphql.buildSchema(
+    fs.readFileSync('./src/schema.graphql', { encoding: 'utf8' })
+);
 
 app.use('/', graphqlHTTP({
   schema: myGraphQLSchema,
