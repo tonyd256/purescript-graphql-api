596bd2858a4b93de8e77678596070ca9b415152b
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2018-02-06 15:00:22 -0800

    Clean up echo function

--- src/API/API.purs
+++ src/API/API.purs
@@ -1,22 +1,14 @@
 module API where
 
 import Prelude
-import Control.Monad.Except (runExcept)
-import Data.Either (Either(..))
-import Data.Foreign (Foreign, renderForeignError, readString)
-import Data.Foreign.Index ((!))
+import Data.Foreign (Foreign)
 import Data.Function.Uncurried (Fn2, mkFn2)
-import Data.List.NonEmpty (head)
 
 test :: Boolean
 test = true
 
-type Input = Foreign
 type Context = Foreign
-type Resolver a = Fn2 Input Context a
+type Resolver input a = Fn2 input Context a
 
-echo :: Resolver String
-echo = mkFn2 $ \input context ->
-  case runExcept (readString =<< input ! "text") of
-       Left errors -> renderForeignError $ head errors
-       Right x -> x
+echo :: Resolver { text :: String } String
+echo = mkFn2 $ \input context -> input.text
